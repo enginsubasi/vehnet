64f35579c6740d859ed7991f357b0ca29bd326af
Engin <enginsubasi@gmail.com>
2024-03-11 22:11:35 +0300

    + Created to read demo file

---
+++ miscScripts/demoOTIDS/demoOtidsMain.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Engin Subasi
+% enginsubasi@gmail.com
+% github.com/enginsubasi
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+close all;
+clear;
+clc;
+
+text = fileread('Attack_free_dataset_10k.txt');
+
+textLines = splitlines(text);
